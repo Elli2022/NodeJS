fd4c50fccdb59da6e7cb5b56c77550287629f2a2
Eleonora Nocentini Sköldebrink <112484819+Elli2022@users.noreply.github.com>
2023-10-10 14:16:56 +0200

    push

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import * as dotenv from "dotenv";
+dotenv.config()
+
+import * as express from "express";
+import helmet from 'helmet';
+import * as cors from 'cors';
+import * as compression from 'compression';
+
+
+const app = express();
+const hostname = process.env.NODE_HOSTNAME;
+const port = process.env.NODE_PORT;
+
+app.use(helmet());
+app.options('*', cors({credentials: true, origin: true}));
+app.use(cors());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+
+// inom res.json({ data: så kan vi "scrapa " en sida och lägga in vilken information som vi vill
+
+
+app.get('/hora',(req, res) =>  res.json({ data:    
+    'jjag tror det'
+})  );
+    
+    app.listen(port, hostname, () => {    
+         console.log(`[EXPRESS] Server running at http://${hostname}: ${port}/`) })
