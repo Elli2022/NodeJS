b4018eb4c1a98693204b1978c0750b978940cfa7
Eleonora Nocentini Sköldebrink <eleonora.nocentini@gmail.com>
2026-05-30 18:31:04 +0200

    Version: testing (2023-10-21)

---
+++ src/app/component/use-cases/index.ts
@@ -0,0 +1,29 @@
+import { access, mkdir, writeFile, readFile } from "node:fs/promises";
+import createPost from "./post";
+import createGet from "./get";
+import config from "../../../tests/config";
+import { logger } from "../../libs/logger";
+
+const fileDirName = config.FILE_FOLDER_NAME;
+const fileDirPath = config.FILE_FOLDER_PATH;
+const filename = config.FILE_DB_NAME;
+const filePath = config.FILE_DB_PATH;
+const errorMsgs = config.ERROR_MSG;
+
+const post = ({ params }) =>
+  createPost({
+    access,
+    mkdir,
+    writeFile,
+    readFile,
+    logger,
+  }).post({ params, filename, fileDirPath, fileDirName, filePath, errorMsgs });
+
+const get = ({ params }) =>
+  createGet({
+    access,
+    readFile,
+    logger,
+  }).get(params, filePath, filename); // Här lägger vi till alla förväntade parametrar
+
+export { post, get };
